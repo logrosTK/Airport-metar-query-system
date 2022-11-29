e3d6f695cf0603f21fa6d730a815df283880189f
TIAN_K <70078768+logrosTK@users.noreply.github.com>
2022-11-29 10:09:44 +0800

    程序的源码，使用python编写

---
+++ METAR.py
@@ -0,0 +1,22 @@
+import webbrowser as web
+from bs4 import BeautifulSoup
+import io
+import sys
+from urllib import request
+#显示系统名称
+print("         气象报文查询系统")
+print("          数据源：VATPRC")
+a=str(input("请输入机场四字码："))
+print("正在查询，请等待......")
+b=str("https://metar.vatsim.net/metar.php?id=")
+c=b+a
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')		#改变标准输出的默认编码
+if __name__ == "__main__":
+	response = request.urlopen(c)
+	html = response.read()
+	html = html.decode('utf-8')		#根据网页的编码方式进行解码
+	print(html)
+	print("--------------------------------------")
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')   #改变标准输出的默认编码改回去
+f=str(input("输入任意值关闭程序："))
+
